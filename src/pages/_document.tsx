f542f81e616311d1cd00a5fd070b25135992dbd3
Eduardy Lopes de Morais <eduardylopes@gmail.com>
2022-01-23 10:21:57 -0300

    Fixed favicon error 404

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,11 @@ export default class MyDocument extends Document {
 				<Head>
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link href="https://fonts.googleapis.com/css2?family=Inter&amp;family=Lexend:wght@500;600&amp;display=swap" rel="stylesheet" />
-					<link rel="shortcut icon" href="favicon.png" type="image/x-icon"/>
+
+					<link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
 				</Head>
 				<body>
 					<Main />
